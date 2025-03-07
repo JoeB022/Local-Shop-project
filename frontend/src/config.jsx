9dc6660e2d2b85168c35ff190f9486eb88baf650
roselyne mwaniki <roselynewairimu30@gmail.com>
2025-03-08 02:00:05 +0300

    edited files

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,1 +1,1 @@
-export const API_URL = 'http://127.0.0.1:5000'; // Replace with your actual API URL
+export const API_URL = 'https://local-shop-project-n6oj.onrender.com'; // Replace with your actual API URL
